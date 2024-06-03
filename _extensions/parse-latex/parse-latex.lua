e3a0b2e9a9b2c2d0b9cafb66e819e0d70bac7e4f
amael-ls <amael.lesquin@gmail.com>
2024-06-03 21:31:47 +0200

    Added the possibility to ignore some environments. Note that when there are nested env it does not work... Such as \eqref in a table.

--- _extensions/parse-latex/parse-latex.lua
+++ _extensions/parse-latex/parse-latex.lua
@@ -3,6 +3,8 @@
 --- Copyright: © 2021–2022 Albert Krewinkel
 --- License: MIT – see LICENSE for details
 
+--- Amael Le Squin: Added the possibility to ignore some environments
+
 -- Makes sure users know if their pandoc version is too old for this
 -- filter.
 PANDOC_VERSION:must_be_at_least '2.9'
@@ -11,6 +13,42 @@ PANDOC_VERSION:must_be_at_least '2.9'
 -- passed through unchanged.
 if FORMAT:match 'latex' then
   return {}
+end
+
+-- Function to read environments that should NOT be treated
+local neglectEnv = pandoc.MetaMap({})
+local beginEnvIgnore = {}
+local backslashEnvIgnore = {}
+
+function Meta(meta)
+  for key, value in pairs(meta["parse-latex"]) do
+    neglectEnv[key] = value
+    if pandoc.utils.stringify(value) == "begin" then
+      beginEnvIgnore[#beginEnvIgnore + 1] = tostring(key)
+    end
+    if pandoc.utils.stringify(value) == "backslash" then
+      backslashEnvIgnore[#backslashEnvIgnore + 1] = tostring(key)
+    end
+  end
+end
+
+-- Helper to check if an environment is listed in toIgnore
+local function ignore(env)
+  for _, value in ipairs(beginEnvIgnore) do
+    value = "\\begin{" .. value .. "}"
+    if string.match(env, value) then
+      return true
+    end
+  end
+
+  for _, value in ipairs(backslashEnvIgnore) do
+    value = "\\" .. value .. "{"
+    if string.match(env, value) then
+      return true
+    end
+  end
+
+  return false
 end
 
 -- Parse and replace raw TeX blocks, leave all other raw blocks
@@ -25,8 +63,17 @@ end
 -- elements alone.
 function RawInline(raw)
   if raw.format:match 'tex' then
+    if ignore(raw.text) then
+      return raw
+    end
     return pandoc.utils.blocks_to_inlines(
       pandoc.read(raw.text, 'latex').blocks
     )
   end
 end
+
+return {
+  { Meta = Meta },
+  { RawBlock = RawBlock },
+  { RawInline = RawInline }
+}
